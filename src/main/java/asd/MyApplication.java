ddc74fc661bacd6d60cd54f1f987bb683561cbcd
bitoogii <bitoogii@yahoo.com>
2024-08-14 21:44:07 -0500

    fix on application

--- src/main/java/asd/MyApplication.java
+++ src/main/java/asd/MyApplication.java
@@ -9,120 +9,72 @@ import framework.annotation.Qualifier;
 import framework.event.ApplicationEventPublisher;
 import framework.util.Runnable;
 import framework.util.Scheduler;
+
 import java.util.Arrays;
 
 
 public class MyApplication implements Runnable {
 
+    @Autowired
+    private AdvertisementService advertisementService;
+    @Autowired
+    private MyScheduledService myScheduledService;
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+    @Autowired
+    @Qualifier("stripePaymentService")
+    private static PaymentService paymentService;
+    @Autowired
+    private EmailService emailService;
 
-//	@Autowired
-//	private MyServiceOne myServiceOne;
-//	@Autowired
-//	private MyScheduledService myScheduledService;
-//	@Autowired
-//	private ApplicationEventPublisher eventPublisher;
-//	@Autowired
-//	@Qualifier("stripePaymentService")
-//	private static PaymentService paymentService;
-//	@Autowired
-//	private MyServiceOne iService;
-//	@Autowired
-//	private EmailService emailService;
-//
-//	@Autowired
-//	AppConfig appConfig;
-//	@Autowired
-//	private ITestConstructor testConstrcutor;
+    @Autowired
+    private IContactService iContactService;
+    @Autowired
+    private ICustomerService iCustomerService;
 
+    public static void main(String[] args) throws Exception {
+        SimpleJavaFrameworkContext context = SimpleJavaFramework.run(MyApplication.class, args);
 
-	@Autowired
-	private AdvertisementService advertisementService;
-	@Autowired
-	private MyScheduledService myScheduledService;
-	@Autowired
-	private ApplicationEventPublisher eventPublisher;
-	@Autowired
-	@Qualifier("stripePaymentService")
-	private static PaymentService paymentService;
-	@Autowired
-	private EmailService emailService;
+        // Schedule tasks using the Scheduler
+        Scheduler scheduler = new Scheduler();
+        context.getBean(AdvertisementService.class).print(); // Example usage of getBean
 
+        DevService devService = context.getBean(DevService.class);
+        ProdService prodService = context.getBean(ProdService.class);
 
-	@Autowired
-	private IContactService iContactService;
-	@Autowired
-	private ICustomerService iCustomerService;
-	public static void main(String[] args) throws Exception{
-		SimpleJavaFrameworkContext context = SimpleJavaFramework.run(MyApplication.class, args);
+        if (devService != null) {
+            devService.print(); // Should print "Development Service" if profile is "dev"
+        }
+        if (prodService != null) {
+            prodService.print(); // Should not be available if profile is "dev"
+        }
+        MyScheduledService scheduledService = context.getBean(MyScheduledService.class);
 
-//		// Schedule tasks using the Scheduler
-//		Scheduler scheduler = new Scheduler();
-//		context.getBean(MyServiceOne.class).print(); // Example usage of getBean
-//
-//		DevService devService = context.getBean(DevService.class);
-//		ProdService prodService = context.getBean(ProdService.class);
+        // Initialize event publisher
+        ApplicationEventPublisher eventPublisher = context.getEventPublisher();
 
-		// Schedule tasks using the Scheduler
-		Scheduler scheduler = new Scheduler();
-		context.getBean(AdvertisementService.class).print(); // Example usage of getBean
+        // Publish a custom event
+        eventPublisher.publishEvent(new CustomEvent("Hello from Custom Event!"));
+        paymentService.processPayment();
+
+        // Ensure scheduler shutdown is called on application exit
+        Runtime.getRuntime().addShutdownHook(new Thread(scheduler::shutdown));
 
 
-		DevService devService = context.getBean(DevService.class);
-		ProdService prodService = context.getBean(ProdService.class);
+    }
 
-		if (devService != null) {
-			devService.print(); // Should print "Development Service" if profile is "dev"
-		}
-		if (prodService != null) {
-			prodService.print(); // Should not be available if profile is "dev"
-		}
-//		// Use getBean to get MyScheduledService and schedule it
-		MyScheduledService scheduledService = context.getBean(MyScheduledService.class);
-		//scheduler.schedule(scheduledService);
+    @Override
+    public void run(String... args) throws Exception {
 
-		// Initialize event publisher
-		ApplicationEventPublisher eventPublisher = context.getEventPublisher();
+        iContactService.addContact("012-345-678", "johndoe@miu.edu");
+        iCustomerService.addCustomer("John", "Dow");
 
-		// Publish a custom event
-		eventPublisher.publishEvent(new CustomEvent("Hello from Custom Event!"));
-		paymentService.processPayment();
+        advertisementService.print();
+        System.out.println(Arrays.toString(args));
 
-		// Keep the application running to observe scheduled tasks
-		//Thread.currentThread().join();
+        iContactService.addContact("012-345-678", "johndoe@miu.edu");
 
-		// Ensure scheduler shutdown is called on application exit
-		Runtime.getRuntime().addShutdownHook(new Thread(scheduler::shutdown));
+        System.out.println("Main thread finished: " + Thread.currentThread().getName());
 
-
-
-}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-//		//myServiceOne.print();
-//		//System.out.println(Arrays.toString(args));
-//		iService.print();
-//		System.out.println(Arrays.toString(args));
-//
-//		System.out.println(appConfig.toString());
-//
-//		emailService.sendEmail();
-//		System.out.println("Main thread finished: " + Thread.currentThread().getName());
-//		//testConstrcutor.test();
-//		System.out.println("Anoj");
-		iContactService.addContact("012-345-678", "johndoe@miu.edu");
-		iCustomerService.addCustomer("John", "Dow");
-
-		advertisementService.print();
-		System.out.println(Arrays.toString(args));
-
-
-		iContactService.addContact("012-345-678", "johndoe@miu.edu");
-
-
-		//emailService.sendEmail();
-		System.out.println("Main thread finished: " + Thread.currentThread().getName());
-
-	}
+    }
 }
